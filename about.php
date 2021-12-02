51e8fa0a84624666772bf1983a0e4533ba352841
Carl <65150183+cndagangon@users.noreply.github.com>
2021-12-02 21:30:34 +0800

    about carl

--- about.php
+++ about.php
@@ -14,14 +14,22 @@
       <div class="col-md-7">
         <h3 class="featurette-heading">The Team.</h3>
         <p class="lead">
-          We are Information Technology students from University of Sto. Tomas,
+          We are Information Technology students from University of Santo Tomas,
           specializing in Web and Mobile Development.
         </p>
         <h6>Carl Dagañgon</h6>
         <p>
-          Proficient in Javascript frameworks for building websites and mobile
-          applications such as React and Angular. Interests include watching
-          movies, listening to music, and playing online games.
+          Advanced knowledge in Programming languages such as Java, JavaScript, 
+          PHP, Python, React Native Framework. Loves the beach, watch tv series and playing games.
+        </p>
+        <p>
+          <a
+            href="https://www.linkedin.com/in/carldagañgon"
+            class="link-secondary"
+            >LinkedIn</a
+          >
+          |
+          <a href="https://github.com/cndagangon" class="link-secondary">GitHub</a>
         </p>
         <h6>Louisse Evale</h6>
         <p>
